a91c8593e00e41e3d330019f743a8b8e77c20958
Emilio Blanco <emilioblanc216@gmail.com>
2023-04-16 12:32:29 -0600

    Implementacion de GIt Ignore

---
+++ myproject/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <input type="text" style={{color: "black"}}/>
+    </div>
+  )
+}
+
+export default page
